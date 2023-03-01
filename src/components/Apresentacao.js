4bc30d21caab24de93307989c973e6d280747af2
Mateus Alcantara <mateus.e.alcantara@outlook.com>
2023-03-01 17:09:34 -0300

    alterações na seção de apresentação

--- src/components/Apresentacao.js
+++ src/components/Apresentacao.js
@@ -5,7 +5,8 @@ function Apresentacao() {
                 <h1 className="text-center w-100">Mateus Evangelista</h1>
                 <div className="row">
                     <p className="col-md-8 ms-auto me-auto text-center d-flex flex-column">
-                        <span>Seja bem-vindo(a). Busco trabalho com Python ou desnvolvimento web.</span>
+                        <span>Seja bem-vindo(a). Meu nome é Mateus e busco trabalho como estagiário ou desenvolvedor júnior.</span>
+                        <span>Busco atuar no front-end ou no back-end.</span>
                         <span>Aqui, você pode ver meus projetos e entrar em contato comigo.</span>
                     </p>
                 </div>
