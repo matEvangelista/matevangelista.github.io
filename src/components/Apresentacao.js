41ce09035622b29b210107f6a3c1ad059b629466
Mateus Alcântara <mateus.e.alcantara@outlook.com>
2024-01-14 21:01:28 -0300

    grafos

--- src/components/Apresentacao.js
+++ src/components/Apresentacao.js
@@ -7,9 +7,7 @@ function Apresentacao() {
                 <h1 className="text-center w-100">Mateus Evangelista</h1>
                 <div className="row">
                     <p className="col-10 ms-auto me-auto text-center d-flex flex-column">
-                        <span>Olá. Meu nome é Mateus e busco trabalho como desenvolvedor júnior ou estagiário.</span>
-                        <span>Posso trabalhar com front-end, Python e Java.</span>
-                        <span>Aqui, você pode ver meus projetos e entrar em contato comigo.</span>
+                        <span>Olá. Meu nome é Mateus e, nesta página, você encontra meus projetos.</span>
                     </p>
                 </div>
                 <div className="row justify-content-center mt-3">
